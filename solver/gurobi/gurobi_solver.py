b24e0945a1a318f77c096640147386401c282f4d
Leander Kammermeier <82841550+LeanderAK@users.noreply.github.com>
2024-06-06 18:16:27 +0200

    fixed gurobi Solver

--- solver/gurobi/gurobi_solver.py
+++ solver/gurobi/gurobi_solver.py
@@ -8,44 +8,20 @@ class GUROBISolver:
 
 
     def solve(_network: Network):
-        print("TBD")
-        nodes = {
-            "1": {"demand": -9},
-            "2": {"demand": 4},
-            "3": {"demand": 17},
-            "4": {"demand": 1},
-            "5": {"demand": -5},
-            "6": {"demand": -8}
-        }
-
-        arcs = [
-            {"from": "1", "to": "2", "cost": 3, "lower_bound": 0, "upper_bound": 2},
-            {"from": "1", "to": "3", "cost": 5, "lower_bound": 0, "upper_bound": 10},
-            {"from": "1", "to": "5", "cost": 1, "lower_bound": 0, "upper_bound": 10},
-            {"from": "2", "to": "3", "cost": 1, "lower_bound": 0, "upper_bound": 6},
-            {"from": "4", "to": "2", "cost": 4, "lower_bound": 0, "upper_bound": 8},
-            {"from": "4", "to": "3", "cost": 1, "lower_bound": 0, "upper_bound": 9},
-            {"from": "5", "to": "3", "cost": 6, "lower_bound": 0, "upper_bound": 9},
-            {"from": "5", "to": "4", "cost": 1, "lower_bound": 0, "upper_bound": 10},
-            {"from": "5", "to": "6", "cost": 1, "lower_bound": 0, "upper_bound": 6},
-            {"from": "6", "to": "2", "cost": 1, "lower_bound": 0, "upper_bound": 7},
-            {"from": "6", "to": "4", "cost": 1, "lower_bound": 0, "upper_bound": 8},
-        ]
-
         # Create a new model
         m = Model("mincostflow")
 
         # Create variables
         flow = {}
-        for arc in arcs:
-            flow[arc["from"], arc["to"]] = m.addVar(lb=arc["lower_bound"], ub=arc["upper_bound"], obj=arc["cost"], name=f'flow_{arc["from"]}_{arc["to"]}')
+        for arc in _network.arcs:
+            flow[arc.from_node.id, arc.to_node.id] = m.addVar(lb=arc.lower_bound, ub=arc.upper_bound, obj=arc.cost, name=f'flow_{arc.from_node.id}_{arc.to_node.id}')
 
         # Add demand constraints
-        for node in nodes:
+        for node_id, node in _network.nodes.items():
             m.addConstr(
-                quicksum(flow[arc["from"], arc["to"]] for arc in arcs if arc["to"] == node) -
-                quicksum(flow[arc["from"], arc["to"]] for arc in arcs if arc["from"] == node) == nodes[node]["demand"],
-                name=f'demand_{node}'
+                quicksum(flow[arc.from_node.id, arc.to_node.id] for arc in _network.arcs if arc.to_node == node) -
+                quicksum(flow[arc.from_node.id, arc.to_node.id] for arc in _network.arcs if arc.from_node == node) == node.initial_demand,
+                name=f'demand_{node_id}'
             )
 
         # Optimize the model
@@ -54,7 +30,7 @@ class GUROBISolver:
         # Print the results
         if m.status == GRB.OPTIMAL:
             print("Optimal solution found:")
-            for arc in arcs:
-                print(f'Flow on arc {arc["from"]} -> {arc["to"]}: {flow[arc["from"], arc["to"]].x}')
+            for arc in _network.arcs:
+                print(f'Flow on arc {arc.from_node.id} -> {arc.to_node.id}: {flow[arc.from_node.id, arc.to_node.id].x}')
         else:
             print("No feasible solution found")
